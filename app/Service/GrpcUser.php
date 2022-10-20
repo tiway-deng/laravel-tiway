31f567954ed734c167d8a49a708586592be97566
tiway <501620409@qq.com>
2022-10-20 17:30:03 +0800

    add user password check

--- app/Service/GrpcUser.php
+++ app/Service/GrpcUser.php
@@ -6,11 +6,13 @@ namespace App\Service;
 
 use Grpc\ChannelCredentials;
 use User\V1\CreateUserInfo;
+use User\V1\IdRequest;
+use User\V1\PasswordCheckInfo;
 use User\V1\UserClient;
 
 class GrpcUser
 {
-    private static function CreateClient()
+    private static function createClient()
     {
         //docker 请求宿主机
         return new UserClient("host.docker.internal:9001", [
@@ -27,7 +29,7 @@ class GrpcUser
         $request->setNickname($nickname);
         $request->setMobile($mobile);
         $request->setPassword($password);
-        $call = self::CreateClient()->CreateUser($request);
+        $call = self::createClient()->CreateUser($request);
         list($response, $status) = $call->wait();
 
         $user = [];
@@ -45,6 +47,53 @@ class GrpcUser
         }
 
         return $user;
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     */
+    public static function getUserById(int $id)
+    {
+        $request = new IdRequest();
+        $request->setId($id);
+        $call = self::createClient()->GetUserById($request);
+        list($response, $status) = $call->wait();
+
+        $user = [];
+        if ($response) {
+            $user = [
+                'id' => $response->getId(),
+                'name' => $response->getName(),
+                'mobile' => $response->getMobile(),
+                'nickname' => $response->getNickname(),
+                'email' => $response->getEmail(),
+                'password' => $response->getPassword(),
+                'status' => $response->getStatus(),
+                'created_at' => $response->getCreatedAt(),
+            ];
+        }
+
+        return $user;
+    }
+
+    /**
+     * @param string $mobile
+     * @param string $password
+     * @return bool
+     */
+    public static function checkoutUserPassword(string $mobile,string $password)
+    {
+        $request = new PasswordCheckInfo();
+        $request->setMobile($mobile);
+        $request->setPassword($password);
+        $call = self::createClient()->CheckPassword($request);
+        list($response, $status) = $call->wait();
+
+        if ($response && $response->getSuccess()) {
+            return true;
+        }
+        return false;
     }
 
 }
